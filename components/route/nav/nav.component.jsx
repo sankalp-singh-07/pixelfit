b3d56c803f82c32a186540118a8d6556a201c002
First Project Demo <sankalp.singh.somvanshi@gmail.com>
2023-12-27 20:45:26 +0530

    Importing Link

--- components/route/nav/nav.component.jsx
+++ components/route/nav/nav.component.jsx
@@ -1,9 +1,18 @@
 import { Outlet } from "react-router-dom" //> use krte hai iska uss jagah pe jaha pe hume child route ko render krna hai jo match ho jaye... child is the nested route inside the parent route
 
+import { Link } from "react-router-dom" //> link works same as anchor tag but it does not reload the page when clicked and works for routing
+
+import "./nav.styles.scss"
+
 const Nav = () => {
     return(
       <>
-      <h1>Nav</h1>
+      <div className="navigation">
+        <h1>LOGO</h1>
+        <div className="nav-links-container">
+            <Link className='nav-link' to="/shop">Shop</Link>
+        </div>
+      </div>
       <Outlet />
       </>
     )
