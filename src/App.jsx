c3667d67b0da15c251fb8c03b6e6268f04f87421
First Project Demo <sankalp.singh.somvanshi@gmail.com>
2023-12-27 16:35:59 +0530

    Routes and Route added to Homepage(root)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from "react-router-dom" //> Routes is a map of all the route in the app and route is a single road of all the roads in routes which tells the app to load a component when a particular route is hit
+
 import Home from "../components/route/homepage/home.component"
 
 const App = () => {
   return(
     <>
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+    </Routes>
     </>
   )
 }
 
 export default App
+
+
+//? path="/" is the root route and element is the component to be loaded when the root route is hit such as www.example.com === www.example.com/ both are same
