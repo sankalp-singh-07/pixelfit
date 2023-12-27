af277bea3db534e2efa2ec7bf8bebda43ba10275
First Project Demo <sankalp.singh.somvanshi@gmail.com>
2023-12-27 13:54:05 +0530

    BrowserRouter import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' //> BrowserRouter keeps UI in sync with URL
 import App from './App.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
   </React.StrictMode>,
 )
+//> Browserrouter is a component that we wrap around our application so that our application gets all the functionality of routing
