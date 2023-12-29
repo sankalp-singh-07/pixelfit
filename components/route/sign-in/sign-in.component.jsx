baa63ce83c0f5e335bd8576c0cdb0d134838a4ec
First Project Demo <sankalp.singh.somvanshi@gmail.com>
2023-12-29 11:01:52 +0530

    Sign In with Popup Completed

--- components/route/sign-in/sign-in.component.jsx
+++ components/route/sign-in/sign-in.component.jsx
@@ -1,21 +1,21 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../../src/utils/firebase/firebase.utils.js"
-
+//> auth can be interpret as a tracker of user authentication status for both browser and firebase
+import {
+	signInWithGooglePopup,
+	createUserDocumentFromAuth,
+} from '../../../src/utils/firebase/firebase.utils.js';
 
 const SignIn = () => {
+	const signInWithPopup = async () => {
+		const { user } = await signInWithGooglePopup(); //> user is the object we need that contains uid and we get from auth
+		const userRef = await createUserDocumentFromAuth(user); //> we pass user object to this function to store data in firestore
+	};
 
-    const signInWithPopup = async() => {
-        const { user } = await signInWithGooglePopup(); //> user is the object we need that contains uid and we get from auth 
-        const userDocRef = await createUserDocumentFromAuth(user); //> we pass user object to this function to store data in firestore
-    }
-
-    return(
-        <>
-            <h1>Sign In Page</h1>
-            <button onClick={signInWithPopup}>
-                Sign in using Google
-            </button>
-        </>
-    )
-}
+	return (
+		<>
+			<h1>Sign In Page</h1>
+			<button onClick={signInWithPopup}>Sign in using Google</button>
+		</>
+	);
+};
 
 export default SignIn;
