c66f3e6a30ec08679be60974026606662ef6f9b7
First Project Demo <sankalp.singh.somvanshi@gmail.com>
2023-12-29 23:44:27 +0530

    Authentication Component styling

--- components/route/authentication/authentication.component.jsx
+++ components/route/authentication/authentication.component.jsx
@@ -6,6 +6,8 @@ import {
 import SignUpForm from '../../sign-up-form/sign-up-form.component.jsx';
 import SignInForm from '../../sign-in-form/sign-in-form.component.jsx';
 
+import './authentication.styles.scss';
+
 const Authentication = () => {
 	// const signInWithPopup = async () => {
 	// 	const { user } = await signInWithGooglePopup(); //> user is the object we need that contains uid and we get from auth and we do destructuring here because we need only user object from the returned object
@@ -13,11 +15,10 @@ const Authentication = () => {
 	// };
 
 	return (
-		<>
-			<h1>Sign In Page</h1>
+		<div className='authentication-container'>
 			<SignInForm />
 			<SignUpForm />
-		</>
+		</div>
 	);
 };
 
